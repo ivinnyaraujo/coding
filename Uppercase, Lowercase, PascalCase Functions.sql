4fbcf8bc1670c11b632c9ab7623867053a399f6c
Ivinny Araujo <93143603+ivinnyaraujo@users.noreply.github.com>
2023-02-04 17:12:11 +1300

    Case conversion functions in SQL Server
    Case conversion functions in SQL Server (Uppercase, Lowercase, PascalCase)

--- Uppercase, Lowercase, PascalCase Functions.sql
+++ Uppercase, Lowercase, PascalCase Functions.sql
@@ -3,5 +3,5 @@
 
   SELECT LOWER ([CompanyName]) AS CompanyName_lowercase
         ,UPPER ([CompanyName]) AS CompanyName_UPPERCASE
-		,UPPER(SUBSTRING([CompanyName], 1, 1)) + LOWER(SUBSTRING([CompanyName], 2, LEN([CompanyName]))) AS CompanyName_PascalCase
+	,UPPER(SUBSTRING([CompanyName], 1, 1)) + LOWER(SUBSTRING([CompanyName], 2, LEN([CompanyName]))) AS CompanyName_PascalCase
   FROM [Training_dbo].[dbo].[Supplier]
