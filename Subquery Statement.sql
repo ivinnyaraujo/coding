f28ba71a25c48c2e7f8a80ba1e7c1402b3fbd9c5
Ivinny Araujo <93143603+ivinnyaraujo@users.noreply.github.com>
2023-02-05 14:04:01 +1300

    Subquery Statement

--- Subquery Statement.sql
+++ Subquery Statement.sql
@@ -2,13 +2,14 @@
 --- Summary: Subquery Statement
 
 --- Context: A Subquery is a query within another SQL query and embedded within the WHERE clause. It is used to return data that will be used in
----			 the main query as a condition to restrict the data to be retrieved.
+---	     the main query as a condition to restrict the data to be retrieved.
 
 --========================================================================================================================================================================================
----- Code briefing:		There are two tables: 1) contains the value of purchase per customer; 2) contains all the customers. We want to:
----							a) Find the total spent per each customer; 
----							b) Add FirstName and LastName attributes in a single attribute; 
----							c) Round Total_Purchase values with 2 decimal places
+---- Code briefing:		
+---	There are two tables: 1) contains the value of purchase per customer; 2) contains all the customers. We want to:
+---					a) Find the total spent per each customer; 
+---					b) Add FirstName and LastName attributes in a single attribute; 
+---					c) Round Total_Purchase values with 2 decimal places
 --================================================================================================
 
 SELECT 
